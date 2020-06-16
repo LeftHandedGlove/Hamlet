a1c49105bddeb0756cbab8c9ec7f64f0c3e3e205
David Delamater <david.w.delamater@gmail.com>
2020-06-15 21:16:44 -0500

    Adding database monitor

---
+++ database-monitor/database_monitor.py
@@ -0,0 +1,53 @@
+import mysql.connector
+import atexit
+import yaml
+
+
+class HamletMySQLDataBase:
+    def __init__(self, address, username, password, database):
+        self.__connection = None
+        self.__address = address
+        self.__username = username
+        self.__password = password
+        self.__database = database
+        self.cursor = None
+
+    def open_connection(self):
+        self.__connection = mysql.connector.connect(
+            host=self.__address,
+            user=self.__username,
+            password=self.__password,
+            database=self.__database
+        )
+        self.cursor = self.__connection.cursor()
+        atexit.register(self.close_connection)
+
+    def close_connection(self):
+        self.__connection.close()
+        self.__connection = None
+        self.cursor = None
+        atexit.unregister(self.close_connection)
+
+
+class SystemStateMonitorDB(HamletMySQLDataBase):
+    def __init__(self):
+        super(SystemStateMonitorDB, self).__init__("localhost", "hamlet", "hamlet", "system_state")
+        self.__expected_system_tables = ["temperature"]
+
+    def add_system_to_database_tables(self):
+        # Check to see if the tables exist
+        self.cursor.execute("SELECT table_name FROM information_schema.tables WHERE table_schema = 'system_state'")
+        result = self.cursor.fetchall()
+        print(result)
+        print(self.cursor)
+
+    def update_table(self, table, columns, values):
+        pass
+        # Construct the query string
+        query = ""
+
+
+if __name__ == "__main__":
+    sys_state_db = SystemStateMonitorDB()
+    sys_state_db.open_connection()
+    sys_state_db.add_system_to_database_tables()
