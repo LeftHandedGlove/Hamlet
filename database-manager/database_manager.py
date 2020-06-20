24b6ae3eee08daf11bc6ffda7bb1a0528feaecd4
DavidDelamater <David.W.Delamater@gmail.com>
2020-06-20 11:41:15 -0500

    Adding code to add missing tables

--- database-manager/database_manager.py
+++ database-manager/database_manager.py
@@ -15,6 +15,11 @@ class MySQLDatabaseConnection:
         self.cursor = None
 
     def open_connection(self):
+        print("Opening connection to database...")
+        print("  Database: {0}".format(self.__database))
+        print("  Address:  {0}".format(self.__address))
+        print("  User:     {0}".format(self.__username))
+        print("  Password: {0}".format(self.__password))
         self.__connection = mysql.connector.connect(
             host=self.__address,
             user=self.__username,
@@ -25,6 +30,7 @@ class MySQLDatabaseConnection:
         atexit.register(self.close_connection)
 
     def close_connection(self):
+        print("Closing connection to database")
         self.__connection.close()
         self.__connection = None
         self.cursor = None
@@ -38,6 +44,7 @@ class DatabaseManager:
         self.__running = False
 
     def start(self):
+        print("Starting Database Manager")
         self.__import_database_config()
         self.__db_connection = MySQLDatabaseConnection(
             self.__db_address, 
@@ -51,6 +58,7 @@ class DatabaseManager:
         atexit.register(self.stop)
 
     def stop(self):
+        print("Stopping Database Manager")
         self.__running = False
         self.__db_connection.close_connection()
         atexit.unregister(self.stop)
@@ -64,6 +72,7 @@ class DatabaseManager:
             self.run_once()
 
     def __import_database_config(self):
+        print("Loading Database Config: {0}".format(self.__db_config_path))
         # Parse the database config file
         with open(self.__db_config_path) as yaml_file:
             db_config_data = yaml.load(yaml_file, Loader=yaml.FullLoader)
@@ -74,21 +83,21 @@ class DatabaseManager:
         self.__db_user = db_config_data['user']
         self.__db_password = db_config_data['password']
         self.__db_tables = db_config_data['tables']
-        print("Loading Database Config '{0}'".format(self.__db_config_path))
-        print("  Database: {0}".format(self.__db_name))
-        print("  Address:  {0}".format(self.__db_address))
-        print("  User:     {0}".format(self.__db_user))
-        print("  Password: {0}".format(self.__db_password))
 
     def __setup_database(self):
-        # Make sure the database exists
-        self.__db_connection.cursor.execute('SHOW DATABASES LIKE "{0}"'.format(self.__db_name))
-        results = self.__db_connection.cursor.fetchall()
-        print(results)
+        print("Setting up database: {0}".format(self.__db_name))
         # Iterate over all of the tables
         for table in self.__db_tables.keys():
             # Make sure the table exists
-            pass
+            self.__db_connection.cursor.execute("SHOW TABLES LIKE '{0}'".format(table))
+            all_results = self.__db_connection.cursor.fetchall()
+            if len(all_results) == 0:
+                print("Unable to find table '{0}', creating it".format(table))
+                create_table_sql = "CREATE TABLE {0}(".format(table)
+                for col_name, col_config in self.__db_tables[table]['columns'].items():
+                    create_table_sql += "{0} {1},".format(col_name, col_config)
+                create_table_sql = create_table_sql[:-1] + ")"
+                self.__db_connection.cursor.execute(create_table_sql)
     
     def __remove_old_data(self):
         pass
