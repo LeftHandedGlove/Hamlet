e2f93ae30125a9ad3ded97245b85f3ad69d84a36
David Delamater <david.w.delamater@gmail.com>
2020-07-10 04:28:11 -0500

    Updating add hamlet user to be better

--- create_hamlet_user.sh
+++ create_hamlet_user.sh
@@ -1,3 +1,17 @@
-useradd hamlet
+echo "Creating hamlet user"
+useradd -m hamlet
+
+echo "Enter new password for hamlet user"
 passwd hamlet
+
+echo "Copying pi user's groups"
+entry_count=0
+for group in $(groups pi); do
+	entry_count=$((entry_count+1))
+    if [ $entry_count -gt 3 ]; then
+	echo "  Adding hamlet to group $group"
+        usermod -a -G $group hamlet
+    fi
+done
+
 echo "Reboot the Raspberry Pi, login as 'hamlet', and delete the 'pi' user with 'userdel -r pi'"
