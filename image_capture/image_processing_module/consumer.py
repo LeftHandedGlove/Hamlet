b13271ba5fe54d6f9a23b0070a153d3f6ca8754e
DavidDelamater <David.W.Delamater@gmail.com>
2020-07-11 16:35:18 -0500

    changing ip address to home one

--- image_capture/image_processing_module/consumer.py
+++ image_capture/image_processing_module/consumer.py
@@ -3,7 +3,7 @@ import zmq
 def consumer():
     context = zmq.Context()
     zmq_socket = context.socket(zmq.PULL)
-    zmq_socket.bind('tcp://127.0.0.1:19700')
+    zmq_socket.bind('tcp://192.168.1.200:19700')
     while True:
         received_string = zmq_socket.recv_string()
         print(received_string)
